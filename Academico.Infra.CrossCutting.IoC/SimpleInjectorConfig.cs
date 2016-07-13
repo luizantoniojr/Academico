e4e5f465b64b8cf94a177cee11edc451b72e5a77
Luiz Antônio <luizantoniosilvajr@outlook.com>
2016-07-12 22:04:02 -0300

    Foram feitas as configurações referentes ao Simple Injector.

---
+++ Academico.Infra.CrossCutting.IoC/SimpleInjectorConfig.cs
@@ -0,0 +1,38 @@
+﻿using Academico.Domain.Interfaces.Repository;
+using Academico.Domain.Interfaces.Services;
+using Academico.Domain.Services;
+using Academico.Infra.Data.Repositories;
+using SimpleInjector;
+
+namespace Academico.Infra.CrossCutting.IoC
+{
+    public class SimpleInjectorConfig
+    {
+        public static void RegisterServices(Container container)
+        {
+            //Service
+            container.Register<IAvaliacaoService, AvaliacaoService>();
+            container.Register<ICursoService, CursoService>();
+            container.Register<IDiscenteService, DiscenteService>();
+            container.Register<IDisciplinaMatriculadaService, DisciplinaMatriculadaService>();
+            container.Register<IDisciplinaService, DisciplinaService>();
+            container.Register<IDocenteService, DocenteService>();
+            container.Register<IFaltaService, FaltaService>();
+            container.Register(typeof(IServiceBase<>), typeof(ServiceBase<>));
+            container.Register<ITurmaService, TurmaService>();
+            container.Register<IUsuarioService, UsuarioService>();
+
+            //Repository
+            container.Register<IAvaliacaoRepository, AvaliacaoRepository>();
+            container.Register<ICursoRepository, CursoRepository>();
+            container.Register<IDiscenteRepository, DiscenteRepository>();
+            container.Register<IDisciplinaMatriculadaRepository, DisciplinaMatriculadaRepository>();
+            container.Register<IDisciplinaRepository, DisciplinaRepository>();
+            container.Register<IDocenteRepository, DocenteRepository>();
+            container.Register<IFaltaRepository, FaltaRepository>();
+            container.Register(typeof(IRepositoryBase<>), typeof(RepositoryBase<,>));
+            container.Register<ITurmaRepository, TurmaRepository>();
+            container.Register<IUsuarioRepository, UsuarioRepository>();
+        }
+    }
+}
---
+++ Academico.Services.WebApi/App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,41 @@
+﻿using Academico.Infra.CrossCutting.IoC;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.WebApi;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Academico.Services.WebApi.App_Start
+{
+    public static class SimpleInjectorInitializer
+    {
+        public static void Initialize()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+            InitializeContainer(container);
+            #region Configurações Owin
+            // Necessário para registrar o ambiente do Owin que é dependência do Identity
+            // Feito fora da camada de IoC para não levar o System.Web para fora
+            //container.RegisterPerWebRequest(() =>
+            //{
+            //    if (HttpContext.Current != null && HttpContext.Current.Items["owin.Environment"] == null &&
+            //        container.IsVerifying())
+            //    {
+            //        return new OwinContext().Authentication;
+            //    }
+            //    return HttpContext.Current.GetOwinContext().Authentication;
+            //});
+            //container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            #endregion
+            container.Verify();
+            DependencyResolver.SetResolver(new SimpleInjectorWebApiDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+        }
+
+        private static void InitializeContainer(Container container)
+        {
+            SimpleInjectorConfig.RegisterServices(container);
+        }
+    }
+}
